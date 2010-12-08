d25310d3fe829f0f16ba7c4be03002d87fe8dd6c
Colin Smith <mail.colin.smith@gmail.com>
2010-12-08 08:34:02 -0800

    -Refactored db

--- electioneering.rb
+++ electioneering.rb
@@ -5,50 +5,36 @@ require 'dm-migrations'
 
 set :port, 8080
 
-candidate_names = ['science', 'math']
+candidates = ['science', 'math']
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/electioneering.db")
 
-class Candidates
-    include DataMapper::Resource
-    property :id, Serial
-    property :name, String
-    property :num_votes, Integer
-end
-
-class Voters 
+class Votes 
     include DataMapper::Resource
     property :id, Serial
     property :ip, String
-    property :num_votes, Integer
+    property :candidate, String
 end
 
 DataMapper.finalize
 
-Candidates.auto_upgrade!
+Votes.auto_upgrade!
 
-def display_candidate(candidate_name)
-    "<li><form method='post' action='/#{candidate_name}'>#{candidate_name}<input value='vote' type='submit'/></form></li>"
+def display_candidate(candidate)
+    "<li><form method='post' action='/vote/#{candidate}'>#{candidate}<input value='vote' type='submit'/></form></li>"
 end
 
 get '/' do
-    "<h2>Pick one:</h2><ol>" + candidate_names.map{|candidate_name| display_candidate(candidate_name)}.join('<br/>') + "</ol>"
+    "<h2>Pick one:</h2><ol>" + candidates.map{|candidate| display_candidate(candidate)}.join('<br/>') + "</ol>"
 end	
 
-post '/:name' do
-    name = params[:name]
-    candidate = Candidates.first_or_create(:name => name)
-    p "Your IP address is #{ @env['REMOTE_ADDR'] }"	
-    candidate.num_votes = candidate.num_votes.nil? ? 1 : candidate.num_votes + 1
-    candidate.save!	
-    voter = Voters.first_or_create(:ip => @env['REMOTE_ADDR'])
-    voter.num_votes = voter.num_votes.nil? ? 1 : voter.num_votes + 1
-    voter.save! 
+post '/vote/:candidate' do
+    Votes.create(:ip => @env['REMOTE_ADDR'], :candidate => params[:candidate])
     redirect '/results'
 end
 
 get '/results' do
-    Candidates.all.map{|candidate| "<li>#{candidate.name} = #{candidate.num_votes.to_s}</li>"}.join('<br>')
+    candidates.map{|candidate| "<li>#{candidate} = #{Votes.count(:candidate => candidate).to_s}</li>"}.join('<br>')
 end
 
 
